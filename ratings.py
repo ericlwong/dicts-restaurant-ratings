d166475735d592fccdce1b5af3e661cbc6ccfa9f
Eric Wong <imericlwong@gmail.com>
2023-04-10 13:32:24 -0700

    Finished reading ratings from file & sorting it into dict

--- ratings.py
+++ ratings.py
@@ -1,4 +1,45 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+file = open('scores.txt')
+
+# {'Penne For Your Thoughts' : '4'}
+ratings_dict = {}
+sorted_dict = {}
+
+for line in file:
+    # new line escape character : \n
+    # strip(), lstrip(), rstrip()
+    # will use rstrip()
+    clean_line = line.rstrip()
+    # will split using :
+    # restaurants = ['Penne For Your Thoughts', '4']
+    restaurants = clean_line.split(':')
+
+    # tokenize
+    # list[0]
+    # dict['key'] = value -> if key exists, modify it else add key-value pair to dict
+    # {'Penne For Your Thoughts': '4', 
+    # 'Ramen On Empty': '3', 
+    # 'Spaghettisburg': '3', 
+    # 'Donut You Want Me Baby': '1', 
+    # 'Eclair And Present Danger': '5', 
+    # 'Polenta To Go Around': '2', 
+    # 'In Perfect Hominy': '2', 
+    # 'Ponzu Scheme': '3', 
+    # 'Tamago Never Dies': '5', 
+    # 'Water Pollo': '1', 
+    # 'Loaf Of My Life': '3', 
+    # 'Matzah-Chew-Sits': '4'}
+    ratings_dict[restaurants[0]] = restaurants[1]
+
+# will need .items() to get all restaurant names in a list
+# then, use sorted() (gets us a new list, so will need to assign to new var)
+restaurant_names = ratings_dict.items() #.items() returns a list of tuples
+sorted_names = sorted(restaurant_names)
+
+for name, rating in sorted_names:
+    sorted_dict[name] = rating
+    print(sorted_dict)
+
+        
